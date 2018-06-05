5f51bc3c9593e0ab4178cb22db72ff5778e55cfc
JessPergentino <jessicapergentino@hotmail.com>
2018-06-05 18:52:20 -0300

    Ajuste da tela de login - Aplicação mantem o usuario logado apos fechar o app.

--- app/src/main/java/loovsoft/com/br/studytool/MainActivity.java
+++ app/src/main/java/loovsoft/com/br/studytool/MainActivity.java
@@ -14,12 +14,26 @@ public class MainActivity extends Activity {
     private EditText nomeEstudante;
     private Button botaoIniciar;
     private SecurityPreferences mSecurityPreferences;
+    private Intent intent;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
 
+        mSecurityPreferences = new SecurityPreferences(this);
+        if(!mSecurityPreferences.getStoreString("ESTUDANTE").isEmpty()){
+            intent = new Intent(MainActivity.this, StudyToolActivity.class);
+            startActivity(intent);
+            finish();
+            return;
+        } else {
+            logar();
+        }
+
+    }
+
+    public void logar(){
+        setContentView(R.layout.activity_main);
         this.mSecurityPreferences = new SecurityPreferences(this);
 
         nomeEstudante = findViewById(R.id.MainActivity_NomeEstudanteID);
@@ -31,14 +45,15 @@ public class MainActivity extends Activity {
                 if(nomeEstudante.getText().length() == 0) {
                     Toast.makeText(MainActivity.this, "Por favor, informe seu nome", Toast.LENGTH_SHORT).show();
                 }else {
-                    Intent intent = new Intent(MainActivity.this, StudyToolActivity.class);
+                    intent = new Intent(MainActivity.this, StudyToolActivity.class);
                     mSecurityPreferences.storeString("ESTUDANTE",nomeEstudante.getText().toString());
                     startActivity(intent);
                     finish();
                 }
             }
         });
-
     }
+
+
 
 }
